4cdc7944c1ea95801bca6f49b34f1c4cbbb6d1a7
Caiden <nzcjpalmer3@gmail.com>
2023-02-19 18:07:13 +0000

    FINALE

--- Repeated Years/Program.cs
+++ Repeated Years/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var h = new HashSet<int>();
+        Console.WriteLine("Choose a year");
+        String answer = Console.ReadLine();
+        List<int> list = new List<int>();
+        for (int i = 0; i <= int.Parse(answer); i++)
+        {
+            Char[] jb = i.ToString().ToCharArray();
+            for (int p = 0; p<i.ToString().Length; p++)
+            {
+                for (int j =p+1; j<i.ToString().Length; j++)
+                {
+                    if (jb[p] == jb[j])
+                    {
+                        h.Add(i);
+                    }
+                }
+            }
+        }
+        int[] removeDuplicates = h.ToArray();
+        Console.WriteLine("Out of {0} years, {1} have repeated digits.", answer,removeDuplicates.Count().ToString());
+    }
+}
