dbdad044018392358f5846281b32742d40826f4b
Caiden <nzcjpalmer3@gmail.com>
2023-02-25 16:44:19 +0000

    It is physically impossible for pigs to look up at the sky

--- Morse Code/Program.cs
+++ Morse Code/Program.cs
@@ -1,25 +1,22 @@
-﻿namespace MorseCode
+﻿using System.Text.RegularExpressions;
+using System;
+namespace MorseCode
 {
     class Definitions
     {
         private string[] written;
-        private string[] morse = new string[] { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----", ".-.-.-", "--..--", "..--.." };
-        private string[] english = new string[] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "Stop", ",", "?" };
+        private string[] morse = new string[] { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----", ".-.-.-", "--..--", "..--..", "|", "" };
+        private string[] english = new string[] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "Stop", ",", "?", " ", "" };
         public Definitions(string[] written1)
         {
             this.written = written1;
         }
         public string Translation()
         {
-            string[] result = new string[written.Length];
+            string newRes = "";
             for (int i = 0; i < written.Length; i++)
             {
-                result[i] = english[Array.IndexOf(written, morse[i])];
-            }
-            string newRes = "";
-            foreach (string s in result)
-            {
-                newRes = newRes + "|" + s;
+                newRes += morse[Array.IndexOf(english, written[i])];
             }
             return newRes;
         }
@@ -28,8 +25,8 @@
     {
         static void Main(string[] args)
         {
-            global::System.Console.WriteLine("Write a line of morse code.");
-            string[] morse = Console.ReadLine().Split();
+            global::System.Console.WriteLine("Write a line to convert to morse code.");
+            string[] morse = Regex.Split(Console.ReadLine().ToLower(), string.Empty);
             Definitions morse1 = new Definitions(morse);
             global::System.Console.WriteLine("This translates to: {0}", morse1.Translation());
         }
