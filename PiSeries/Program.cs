ced49a8e52fab4deb0741e2a5780e307ec566810
Caiden Palmer <nzcjpalmer3@gmail.com>
2023-03-13 19:00:49 +0000

    It's illegal to own just one guinea pig in Switzerland

--- PiSeries/Program.cs
+++ PiSeries/Program.cs
@@ -10,6 +10,16 @@ namespace PiSeries
     {
         static void Main(string[] args)
         {
+            double pi = 0;
+            double sign;
+            Console.WriteLine("Choose a number of decimal places: ");
+            int dps = int.Parse(Console.ReadLine());
+            for (int i =0; i < 100000; i++)
+            {
+                sign = Math.Pow(-1, i);
+                pi += (sign * 1 / (2 * i + 1));
+            }
+            Console.WriteLine("Pi to {0} decimal places is {1}", dps, Math.Round(pi, dps));
         }
     }
 }
