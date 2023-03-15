7a282752411b84f12b4c113b414cd45ab78fb1dd
Caiden Palmer <nzcjpalmer3@gmail.com>
2023-03-15 00:02:23 +0000

    The Eiffel Tower can be 15cm taller in the summer

--- GenerateStarsDraw/Form1.cs
+++ GenerateStarsDraw/Form1.cs
@@ -7,7 +7,7 @@ namespace GenerateStarsDraw
 {
     public partial class Form1 : Form
     {
-        SoundPlayer barIncrease = new SoundPlayer(@"C:/Users/nzcjp/Downloads/Shepard-tone.wav");
+        SoundPlayer barIncrease = new SoundPlayer(@"C:/Users/nzcjp/downloads/Shepard-tone1.wav");
         public Random rng = new Random();
         SolidBrush[] colourOptions = new SolidBrush[] { new SolidBrush(Color.White), new SolidBrush(Color.WhiteSmoke), new SolidBrush(Color.FloralWhite), new SolidBrush(Color.AliceBlue), new SolidBrush(Color.LightYellow)};
         public bool continueGeneratingCircles = true;
@@ -30,94 +30,99 @@ namespace GenerateStarsDraw
                 textBox1.Text = String.Empty;
             }
         }
-
-         private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
         {
-            Thread thread2 = new Thread(() =>
+            progressBar1.Invoke(new Action(() =>
             {
-                while (progressBar1.Value < progressBar1.Maximum)
-                {
-                    barIncrease.Play();
-                }
-                barIncrease.Stop();
-            });
-            thread2.Start();
+                progressBar1.Show();
+            }));
             continueGeneratingCircles = true;
             Graphics generateStar = Graphics.FromHwnd(this.Handle);
-            circleGenerationTask = Task.Run(() =>
+            Task soundTask = Task.Run(() =>
             {
-                int i = 0;
-                while (i < int.Parse(textBox1.Text) && continueGeneratingCircles)
+                barIncrease.Play();
+            });
+            circleGenerationTask = Task.Run(() =>
                 {
-                    int size = rng.Next(1, 9);
-                    Point p = new Point();
-                    p.X = rng.Next(0, this.Width);
-                    p.Y = rng.Next(0, this.Height);
-                    int radius = size % 2 == 0 ? size / 2 : (size - 1) / 2;
-                    Rectangle star = new Rectangle(p.X, p.Y, size, size);
-                    int rand = rng.Next(0, 6);
-                    double glowRadiusPercentage = 0.35;
-                    int glowRadius = (int)(radius * glowRadiusPercentage);
-                    if (rand == 0)
+                    int i = 0;
+                    while (i < int.Parse(textBox1.Text) && continueGeneratingCircles)
                     {
-                        Color c = Color.FromArgb(rng.Next(256), rng.Next(256), rng.Next(256), rng.Next(256));
-                        Color glareColor = Color.FromArgb(100, c);
-                        Color glowColor = Color.FromArgb(50, c);
-                        SolidBrush glowBrush = new SolidBrush(glowColor);
-                        Rectangle circleBounds = new Rectangle(star.X, star.Y, size, size);
-                        Rectangle glowBounds = new Rectangle(star.X - glowRadius, star.Y - glowRadius, size + glowRadius * 2, size + glowRadius * 2);
-                        GraphicsPath circlePath = new GraphicsPath();
-                        circlePath.AddEllipse(star);
-                        PathGradientBrush pgb = new PathGradientBrush(circlePath);
-                        pgb.CenterPoint = new PointF(star.X + star.Width / 2, star.Y + star.Height / 2);
-                        pgb.CenterColor = c;
-                        Color[] surroundColors = new Color[7];
-                        for (int j = 0; j < 7; j++)
+                        int size = rng.Next(1, 9);
+                        Point p = new Point();
+                        p.X = rng.Next(0, this.Width);
+                        p.Y = rng.Next(0, this.Height);
+                        int radius = size % 2 == 0 ? size / 2 : (size - 1) / 2;
+                        Rectangle star = new Rectangle(p.X, p.Y, size, size);
+                        int rand = rng.Next(0, 6);
+                        double glowRadiusPercentage = 0.35;
+                        int glowRadius = (int)(radius * glowRadiusPercentage);
+                        if (rand == 0)
                         {
-                            int alpha = (int)((1 - (j / 6.0)) * 100);
-                            surroundColors[j] = Color.FromArgb(alpha, glareColor);
+                            Color c = Color.FromArgb(rng.Next(256), rng.Next(256), rng.Next(256), rng.Next(256));
+                            Color glareColor = Color.FromArgb(100, c);
+                            Color glowColor = Color.FromArgb(50, c);
+                            SolidBrush glowBrush = new SolidBrush(glowColor);
+                            Rectangle circleBounds = new Rectangle(star.X, star.Y, size, size);
+                            Rectangle glowBounds = new Rectangle(star.X - glowRadius, star.Y - glowRadius, size + glowRadius * 2, size + glowRadius * 2);
+                            GraphicsPath circlePath = new GraphicsPath();
+                            circlePath.AddEllipse(star);
+                            PathGradientBrush pgb = new PathGradientBrush(circlePath);
+                            pgb.CenterPoint = new PointF(star.X + star.Width / 2, star.Y + star.Height / 2);
+                            pgb.CenterColor = c;
+                            Color[] surroundColors = new Color[7];
+                            for (int j = 0; j < 7; j++)
+                            {
+                                int alpha = (int)((1 - (j / 6.0)) * 100);
+                                surroundColors[j] = Color.FromArgb(alpha, glareColor);
+                            }
+                            surroundColors[6] = Color.Transparent;
+                            pgb.SurroundColors = surroundColors;
+                            generateStar.FillEllipse(new SolidBrush(c), star);
+                            generateStar.FillEllipse(glowBrush, glowBounds);
+                            generateStar.FillEllipse(glowBrush, circleBounds);
                         }
-                        surroundColors[6] = Color.Transparent;
-                        pgb.SurroundColors = surroundColors;
-                        generateStar.FillEllipse(new SolidBrush(c), star);
-                        generateStar.FillEllipse(glowBrush, glowBounds);
-                        generateStar.FillEllipse(glowBrush, circleBounds);
-                    }
-                    else
-                    {
-                        SolidBrush brush = colourOptions[rng.Next(0, colourOptions.Length)];
-                        Color glareColor = Color.FromArgb(100, brush.Color);
-                        Rectangle circleBounds = new Rectangle(star.X, star.Y, size, size);
-                        Rectangle glowBounds = new Rectangle(star.X - glowRadius, star.Y - glowRadius, size + glowRadius * 2, size + glowRadius * 2);
-                        GraphicsPath circlePath = new GraphicsPath();
-                        circlePath.AddEllipse(star);
-                        PathGradientBrush pgb = new PathGradientBrush(circlePath);
-                        pgb.CenterPoint = new PointF(star.X + star.Width / 2, star.Y + star.Height / 2);
-                        pgb.CenterColor = brush.Color;
-                        Color[] surroundColors = new Color[7];
-                        for (int j = 0; j < 7; j++)
+                        else
                         {
-                            int alpha = (int)((1 - (j / 6.0)) * 100);
-                            surroundColors[j] = Color.FromArgb(alpha, glareColor);
-                        }
-                        surroundColors[6] = Color.Transparent;
-                        pgb.SurroundColors = surroundColors;
-                        Color glowColor = Color.FromArgb(50, brush.Color);
-                        SolidBrush glowBrush = new SolidBrush(glowColor);
-                        generateStar.FillEllipse(new SolidBrush(brush.Color), star);
-                        generateStar.FillEllipse(glowBrush, glowBounds);
-                        generateStar.FillEllipse(glowBrush, circleBounds);
+                            SolidBrush brush = colourOptions[rng.Next(0, colourOptions.Length)];
+                            Color glareColor = Color.FromArgb(100, brush.Color);
+                            Rectangle circleBounds = new Rectangle(star.X, star.Y, size, size);
+                            Rectangle glowBounds = new Rectangle(star.X - glowRadius, star.Y - glowRadius, size + glowRadius * 2, size + glowRadius * 2);
+                            GraphicsPath circlePath = new GraphicsPath();
+                            circlePath.AddEllipse(star);
+                            PathGradientBrush pgb = new PathGradientBrush(circlePath);
+                            pgb.CenterPoint = new PointF(star.X + star.Width / 2, star.Y + star.Height / 2);
+                            pgb.CenterColor = brush.Color;
+                            Color[] surroundColors = new Color[7];
+                            for (int j = 0; j < 7; j++)
+                            {
+                                int alpha = (int)((1 - (j / 6.0)) * 100);
+                                surroundColors[j] = Color.FromArgb(alpha, glareColor);
+                            }
+                            surroundColors[6] = Color.Transparent;
+                            pgb.SurroundColors = surroundColors;
+                            Color glowColor = Color.FromArgb(50, brush.Color);
+                            SolidBrush glowBrush = new SolidBrush(glowColor);
+                            generateStar.FillEllipse(new SolidBrush(brush.Color), star);
+                            generateStar.FillEllipse(glowBrush, glowBounds);
+                            generateStar.FillEllipse(glowBrush, circleBounds);
 
+                        }
+                        i++;
+                        progressBar1.Invoke(new Action(() =>
+                        {
+                            progressBar1.Value = i;
+                        }));
+                        Application.DoEvents();
+                        Thread.Sleep(10);
                     }
-                    i++;
+                    // end of star generation
                     progressBar1.Invoke(new Action(() =>
                     {
-                        progressBar1.Value = i;
+                        progressBar1.Hide();
                     }));
-                    Application.DoEvents();
-                    Thread.Sleep(10);
-                }
-            });
+                    barIncrease.Stop();
+                });
+            await soundTask;
         }
 
         protected override void OnFormClosing(FormClosingEventArgs e)
